303d836c3eae0d35d370fe660df9c85cab931a55
kelvinfernandess04 <kelvin.fernandess04@hotmail.com>
2024-10-09 20:54:21 -0300

    Projeto criado no NetBeans

---
+++ Solo-Quest/src/main/java/br/com/edu/cefsa/SoloQuest/SoloQuestApplication.java
@@ -0,0 +1,13 @@
+package br.com.edu.cefsa.SoloQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloQuestApplication.class, args);
+	}
+
+}
