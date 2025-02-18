6956d46e4998f8c286146baf3b36021a398ab24e
Kelvin <kelvin.fernandess04@hotmail.com>
2025-02-18 19:49:21 -0300

    Projeto reiniciado

--- soloquest/scr/models/AttributeContribution.ts
+++
@@ -1,7 +0,0 @@
-export class AttributeContribution {
-    constructor(
-      public attributeName: "STR" | "AGI" | "VIT" | "INT" | "PER" | "DEX",
-      public contributionValue: number
-    ) {}
-  }
-  
